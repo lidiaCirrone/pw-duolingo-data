30936655d71b97a3c4999d94703b64cc2f9d351b
Laptop <lidia.cirrone@gmail.com>
2021-11-27 00:27:45 +0100

    core: added xp count + refactor

--- pw-duolingo-data.php
+++ pw-duolingo-data.php
@@ -9,10 +9,12 @@ Author: Lidia Cirrone
 Author URI: http://polyglotwannabe.com/
 */
 
+
 function flag_icons_styles()
 {
-   // check if is_page() == 'duolingo'?
-   wp_enqueue_style('flag-icons-main', plugins_url('pw-duolingo-data') . '/flag-icons-main/css/flag-icons.min.css', array(), null);
+   if (is_page('duolingo')) {
+      wp_enqueue_style('flag-icons-main', plugins_url('pw-duolingo-data') . '/flag-icons-main/css/flag-icons.min.css', array(), null);
+   }
 }
 add_action('wp_enqueue_scripts', 'flag_icons_styles');
 
@@ -23,40 +25,23 @@ function duolingo_api_call($attributes)
    $output = shell_exec($command);
    $duo = json_decode($output);
    $current_languages = $duo->current_languages;
-   // var_dump($duo);
-   // {
-   //    ["username"]=> "lidiaCirrone"
-   //    ["streak"] => 1664
-   //    ["learning_language"] => { 
-   //       ["abbr"]=> "sv" 
-   //       ["string"]=>  "Swedish" 
-   //       ["level"]=> ["level_left"]
-   //       ["level_percent"]=> 
-   //    } 
-   //    ["current_languages"]=> {
-   //       [0]=> "it" 
-   //       [1]=> "es" 
-   //       [2]=> "ru" 
-   //       [3]=> "pt" 
-   //       [4]=> "fr"
-   //       [5]=> "de"
-   //       [6]=> "da"
-   //       [7]=> "sv"
-   //    }
-   // }
-
-
-
    ob_start();
 ?>
    <div class="card">
       <div class="card-body small">
          <h5 class="card-title mb-3"><?php echo $duo->username; ?></h5>
          <p class="card-subtitle text-pastel mt-0">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar-week" viewBox="0 0 16 16">
+               <path d="M11 6.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-5 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+               <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z" />
+            </svg>
+            <?php echo $duo->streak; ?> day streak
+         </p>
+         <p class="card-subtitle text-pastel mt-0">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-lightning-charge" viewBox="0 0 16 16">
                <path d="M11.251.068a.5.5 0 0 1 .227.58L9.677 6.5H13a.5.5 0 0 1 .364.843l-8 8.5a.5.5 0 0 1-.842-.49L6.323 9.5H3a.5.5 0 0 1-.364-.843l8-8.5a.5.5 0 0 1 .615-.09zM4.157 8.5H7a.5.5 0 0 1 .478.647L6.11 13.59l5.732-6.09H9a.5.5 0 0 1-.478-.647L9.89 2.41 4.157 8.5z" />
             </svg>
-            <?php echo $duo->streak; ?> day streak
+            <?php echo $duo->xp; ?> total xp
          </p>
          <p class="card-subtitle text-pastel mt-0">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chat-text" viewBox="0 0 16 16">
@@ -69,14 +54,15 @@ function duolingo_api_call($attributes)
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bar-chart" viewBox="0 0 16 16">
                <path d="M4 11H2v3h2v-3zm5-4H7v7h2V7zm5-5v12h-2V2h2zm-2-1a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1h-2zM6 7a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H7a1 1 0 0 1-1-1V7zm-5 4a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1v3a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1v-3z" />
             </svg>
-            level <?php echo $duo->learning_language->level; ?>/<?php echo $duo->learning_language->level_left; ?>
+            level <?php echo $duo->learning_language->level; ?> (<?php echo $duo->learning_language->level_percent; ?>%)
          </p>
          <div class="card-text my-3">
             all the languages I've been studying on Duolingo up until now:<br>
-            <?php foreach ($current_languages as $index => $language) { 
-               if ($language == 'da') : ($language = 'dk'); endif; ?>
+            <?php foreach ($current_languages as $index => $language) {
+               if ($language == 'da') : ($language = 'dk');
+               endif; ?>
                <span class="flag-icon flag-icon-<?php echo $language; ?>"></span>
-               <?php } ?>
+            <?php } ?>
          </div>
          - <a href="https://www.duolingo.com/lidiaCirrone" target="_href" class="card-link">my profile</a><br>
          - <a href="https://duome.eu/lidiaCirrone" target="_href" class="card-link">more statistics on duome.eu</a>
--- pw-load-api.py
+++ pw-load-api.py
@@ -6,24 +6,27 @@ import json, duolingo
 duo_user = duolingo.Duolingo('lidiaCirrone', 'hyaB_3cQN-ei')
 user_info = duo_user.get_user_info()
 username = user_info['username']
+userid = user_info['id']
+
+language_data = user_info['language_data']
 learning_language_abbr = ''
-for lang in user_info['language_data']:
+for lang in language_data:
     learning_language_abbr = lang
+
 language_progress = duo_user.get_language_progress(learning_language_abbr)
+xp = language_data[learning_language_abbr]['points_ranking_data_dict'][str(userid)]['points_data']['total']
 current_languages = duo_user.get_languages(abbreviations=True)
 
 user_object = {
     'username': username,
     'streak': language_progress['streak'],
+    'xp': xp,
     'learning_language': {
-        'abbr': learning_language_abbr,
         'string': language_progress['language_string'],
         'level': language_progress['level'],
-        'level_left': language_progress['level_left'],
         'level_percent': language_progress['level_percent']
     },
     'current_languages': current_languages
 }
 
 print(json.dumps(user_object))
-# print(user_object)
